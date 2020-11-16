f6a2bd35b344068c93b80a919e78d6c17cf3be74
Louis Gautier <louisg404@macbook-pro.home>
2020-11-16 17:01:51 +0100

    feat/ popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,34 @@ export class AppComponent {
 
   ngOnInit() {
     const myMap = L.map('map', {zoomControl: false}).setView([47.2173, -1.5534], 11);
- 
+
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: 'Map'
     }).addTo(myMap);
+
+    var myIcon = L.icon({
+      iconUrl: '../assets/img/ellipse.svg',
+      iconSize: [38, 95],
+      iconAnchor: [22, 94],
+      popupAnchor: [-3, -76],
+      shadowSize: [68, 95],
+      shadowAnchor: [22, 94]
+  });
+
+    L.marker([47.2173, -1.5534], {icon: myIcon}).on('click', this.openPopup).addTo(myMap);
+  }
+
+  closePopup() {
+    const popup = document.getElementById('popup');
+    if(popup) {
+      popup.style.display = 'none';
+    }
+  }
+
+  openPopup() {
+    const popup = document.getElementById('popup');
+    if(popup) {
+      popup.style.display = 'block';
+    }
   }
 }
